268abecfd572288b85f0440c28d164c38a417319
Penny Tague <pmtague9@gmail.com>
2020-05-08 14:53:34 -0700

    Ex 2 complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,9 +1,18 @@
 def get_indices_of_item_weights(weights, length, limit):
 
-    i = 0
-    weight = {}
+    weight_dict = {}
 
-    for i in weights:
-		weight[i] = [i]
+    for i in range (0, length-1):
+        weight_dict[weights[i]] = i
+    # print(weight_dict)
+
+    for x in range (0, weight_dict:
+        difference = limit - weight_dict[x]
+
+        if difference in weight_dict:
+            if weight_dict[difference] > weight_dict[x]:
+                return (weight_dict[difference], weight_dict[x])
+            else:
+                return (weight_dict[x], weight_dict[difference])
 
     return None
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,28 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    
-    """
-    YOUR CODE HERE
-    """
 
+# Initialize an empty dictionary
+    ticket_hash = {}
+# Initialize an empty list to store the final route
+    route = []
+    
+# Input each ticket instance into the dictionary
+    for i in range (0, length):
+        ticket_hash[tickets[i].source] = tickets[i].destination
+    print("Ticket Dictionairy", ticket_hash)
+
+# Retrieve the destination with the source "None" and put it into the first place in the initialized list
+
+    if "NONE" in ticket_hash:
+        route.append(ticket_hash.get("NONE"))
+
+    for t in range (0, length-1):
+        if route[t] in ticket_hash:
+            route.append(ticket_hash.get(route[t]))
+    print("Route", route)
+
+# Retreive the dict entries with the same source as the previous destination (looking like a linked list) and add them to the list in the order they are retrieved
+
+# Return the list (route)
     return route
