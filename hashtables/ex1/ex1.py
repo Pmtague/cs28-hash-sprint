0b2133e395f81c144c669f07b98d8693f0177212
Penny Tague <pmtague9@gmail.com>
2020-05-09 06:21:28 -0700

    Ex1 complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -5,29 +5,27 @@ def get_indices_of_item_weights(weights, length, limit):
 
     # Input the weights as keys and their list indices as values
 
-    for i in range (0, length):
-        weight_dict[weights[i]] = i
-        print("Weights Dictionary", weight_dict)
+    for idx in range (0, length):
+        weight_dict[weights[idx]] = idx
+    print("Weights Dictionary", weight_dict)
 
 
     # Loop through weight_dict, subtracting each key from the limit and checking to see if the difference is in weight_dict
 
-    for x in weight_dict:
-        difference = limit - x
+    for idx in range (0, length):
+        difference = limit - weights[idx]
 
         # If the difference is in weight_dict
         if difference in weight_dict:
 
             # Compare the indeces of the matched item weights
             # Place the larger index in a touple, followed by the smallest
-            if difference == x:
-                return ()
-            if weight_dict[difference] > weight_dict[x]:
-                return (weight_dict[difference], weight_dict[x])
-                print ("Result", weight_dict[difference], weight_dict[x])
+            if weight_dict[difference] > idx:
+                return (weight_dict[difference], idx)
+                print ("Result", weight_dict[difference], idx)
             else:
-                return (weight_dict[x], weight_dict[difference])
-                print ("Other Result", weight_dict[x], weight_dict[difference])
+                return (idx, weight_dict[difference])
+                print ("Other Result", idx, weight_dict[difference])
 
     # If there is no pair of items that max out the limit, return None
     return None
