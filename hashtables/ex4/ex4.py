8785fbc0b8c2850f61ddbe6eff2650b9b891f872
Penny Tague <pmtague9@gmail.com>
2020-05-10 17:01:04 -0700

    Ex4 is passing tests but taking 101 seconds to do it

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,9 +1,33 @@
 def has_negatives(a):
 
-    """
-    YOUR CODE HERE
-    """
+    # Initialize an empty dictionary, result list and temp list
+    positives = {}
+    result = []
+    temp_list = []
 
+    # Loop through the list (a)
+    for idx in range (0, len(a)):
+        pos = a[idx]
+        # Add all positive numbers to the dictionary
+        if pos > 0:
+            positives[pos] = False
+        # Add the negative numbers to a temporary list
+        elif pos < 0:
+            temp_list.append(pos)
+    
+    print("Temp List", temp_list)
+    print("Positives Dict", positives)
+
+    # Loop through the temp list and get the absolute value of each item
+    for ind in range (0, len(temp_list)):
+        p = temp_list[ind]
+
+        # Check the dictionary for the absolute value of each item from the temp list
+        if abs(p) in positives:
+            # If the positive compliment is in the dictionary, add that compliment to the result list
+            result.append(abs(p))
+    print(result)
+    # Return the result
     return result
 
 
